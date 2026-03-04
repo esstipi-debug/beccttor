c567a5bfdcec0bb3fdf4fbf9ac040908887b8172
esstipi-debug <esstipi@gmail.com>
2026-03-04 15:34:52 -0300

    Add BOLT Calculation Services module

---
+++ backend/app/__init__.py
@@ -0,0 +1,27 @@
+"""BOLT Calculation Services"""
+
+from .calculations import (
+    StressEngine,
+    RecoveryCalculator,
+    MenstrualPeriodizationEngine,
+    BoltCoreEngine,
+    ACWRZone,
+    MenstrualPhase,
+    WorkoutData,
+    MovementData,
+    BiometricData,
+    AcwrResult
+)
+
+__all__ = [
+    "StressEngine",
+    "RecoveryCalculator",
+    "MenstrualPeriodizationEngine",
+    "BoltCoreEngine",
+    "ACWRZone",
+    "MenstrualPhase",
+    "WorkoutData",
+    "MovementData",
+    "BiometricData",
+    "AcwrResult"
+]
